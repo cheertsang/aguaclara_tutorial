0ae5f1e539436e82abc8d5776a733d950700677e
cheertsang <ct542@cornell.edu>
2019-02-15 00:01:48 -0500

    update for 2019 spring

--- Interactive-Tutorial-2-Python-Basics.py
+++ Interactive-Tutorial-2-Python-Basics.py
@@ -27,14 +27,14 @@ start_recording()
 #
 # Hint: It's rice and beans. Yum, string beans.
 
-favorite_food = '' # Your answer here
+favorite_food = 'rice and beans' # Your answer here
 
 print(favorite_food)
 
 # TODO 2: Calculate the volume of a 7 x 7 x 5 cube using Python math
 # operations.
 
-volume = 0 # Your answer here
+volume = 7*7*5 # Your answer here
 
 print(volume)
 
@@ -46,8 +46,9 @@ print(volume)
 # in the Writing Python Code tutorial if you're unsure.
 
 hydraulic_processes = ['flocculation', 'sedimentation', 'stirring']
-
-# Your answer here
+hydraulic_processes.pop()
+hydraulic_processes.append('filtration')
+hydraulic_processes[1] = '1 LPS'
 
 print(hydraulic_processes)
 
@@ -56,7 +57,7 @@ print(hydraulic_processes)
 
 analogy = {'knowledge' : 'power', 'France': 'ham'}
 
-# Your answer here
+analogy['France'] = 'bacon'
 
 print(analogy['knowledge'] + ' ' + analogy['France'])
 
@@ -75,7 +76,10 @@ c = False or not False and True or True
 
 does_it_work = 'maybe'
 
-# Your answer here
+if((a or b) and not c):
+    does_it_work = 'yes'
+else:
+    does_it_work = 'no'
 
 print(does_it_work)
 
@@ -87,7 +91,8 @@ print(does_it_work)
 
 x = 1
 
-# Your answer here
+for i in range(1,30):
+    x *= i
 
 print(x)
 
@@ -99,7 +104,11 @@ print(x)
 
 x = 4
 
-# Your answer here
+def squared(num):
+    return num**2
+
+for i in range(6):
+    x = squared(x)
 
 print(x)
 
